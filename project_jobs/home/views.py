49dd9af30ea1f7cbe118a19f51e168e892a35bcb
fatmajanna <fatma.janna.utp@gmail.com>
2019-02-14 19:19:03 +0700

    progress home, about and category

--- project_jobs/home/views.py
+++ project_jobs/home/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def display_home(request):
     return render(request, 'home.html', {})
+
+def display_about(request):
+    return render(request, 'about.html', {})
+
+def display_category(request):
+    return render(request, 'category.html', {})
