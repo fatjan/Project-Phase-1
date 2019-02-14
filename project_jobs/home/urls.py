7eebf612fbed6919036d31a1391b6444df51f680
fatmajanna <fatma.janna.utp@gmail.com>
2019-02-15 00:23:20 +0700

    progress job list with form and read more feature

--- project_jobs/home/urls.py
+++ project_jobs/home/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.display_home, name='home'),
     path('about/', views.display_about, name='about'),
     path('category/', views.display_category, name='category'),
+    path('jobdetail/<int:job_id>', views.job_desc, name='job_desc'),
+    path('job_post/', views.job_new, name='job_new') 
 ]
 
--- project_jobs/home/views.py
+++ project_jobs/home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Job 
+from .employer_form import JobForm
 
 # Create your views here.
 def display_home(request):
@@ -8,4 +10,21 @@ def display_about(request):
     return render(request, 'about.html', {})
 
 def display_category(request):
-    return render(request, 'category.html', {})
+    job = Job.objects.all()
+    return render(request, 'category.html', {'Jobs': job})
+
+def job_desc(request, job_id):
+    job = Job.objects.get(pk=job_id)
+    job.salary = "{:,}".format(job.salary)
+    return render(request, 'job_detail.html', {'job': job})   
+
+def job_new(request):
+    if request.method == "POST":
+        form = JobForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save
+            form.save()
+            return redirect('category')
+    else:
+        form = JobForm()
+    return render(request, 'employer_form.html', {'form': form})
